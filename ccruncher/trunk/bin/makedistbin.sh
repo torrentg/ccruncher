248788eb02fba3b79802922f23fa25a3ca7182b3
gerard <gerard@5129c4fb-51ec-0310-b567-9ce87c476161>
2005-01-16 12:48:36 +0000

    modificacions per la distribucio binaria

--- ccruncher/trunk/bin/makedistbin.sh
+++ ccruncher/trunk/bin/makedistbin.sh
@@ -7,33 +7,51 @@
 # *************************************************************
 
 NAME="ccruncher"
-VERSION="0.1_bin"
+VERSION="0.1"
 
 # -------------------------------------------------------------
 # obtaining repository content
 # -------------------------------------------------------------
 svn checkout http://www.generacio.com/svn/repos/ccruncher/trunk
-mv trunk ccruncher-$VERSION
+mv trunk $NAME-$VERSION
 cd $NAME-$VERSION
+
+# -------------------------------------------------------------
+# creating binaries
+# -------------------------------------------------------------
+aclocal
+autoconf
+automake -avc --warnings=all
+./configure --prefix=$PWD
+make
+make install
 
 # -------------------------------------------------------------
 # dropping unused files
 # -------------------------------------------------------------
-rm configure*
+rm aclocal.m4
+rm -rvf autom4te.cache
+chmod -R +w $NAME-$VERSION
+rm -rvf $NAME-$VERSION
+rm config*
 rm Makefile*
+rm depcomp install-sh missing
 rm INSTALL
 rm bin/clean.sh
 rm bin/makedistbin.sh
 rm -rvf src
 rm -rvf share
 rm -rvf `find . -name \.svn\*`
+cd doc
+rm -rvf `find . ! -name \*.pdf`
+cd ..
 
 # -------------------------------------------------------------
 # making package
 # -------------------------------------------------------------
 cd ..
-tar -cvf $NAME-$VERSION.tar $NAME-$VERSION
-gzip $NAME-$VERSION.tar
+tar -cvf $NAME-${VERSION}_bin.tar $NAME-$VERSION
+gzip $NAME-${VERSION}_bin.tar
 rm -rvf $NAME-$VERSION
 
 # -------------------------------------------------------------
