e70491d7aa61952794d1ce86900c4e9699c90dc9
gerard <gerard@5129c4fb-51ec-0310-b567-9ce87c476161>
2005-06-10 19:58:44 +0000

    version numbers rolled

--- ccruncher/trunk/bin/makedist.sh
+++ ccruncher/trunk/bin/makedist.sh
@@ -20,7 +20,7 @@
 #-------------------------------------------------------------
 progname=makedist.sh
 numversion="0.3"
-svnversion="R171"
+svnversion="R173"
 disttype="src"
 PACKAGE="ccruncher"
 pathexes=""
