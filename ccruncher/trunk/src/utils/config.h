e51ccd25cb80728313b9952edb430ebc7ba94865
gerard <gerard@5129c4fb-51ec-0310-b567-9ce87c476161>
2005-07-23 15:53:09 +0000

    added class BlockMatrixChol (optimized cholesky for client correlation matrix)

--- ccruncher/trunk/src/utils/config.h
+++ ccruncher/trunk/src/utils/config.h
@@ -2,7 +2,7 @@
 /* src/utils/config.h.in.  Generated from configure.in by autoheader.  */
 
 /* build date */
-#define BUILD_DATE "Thu Jul 21 20:06:29 CEST 2005"
+#define BUILD_DATE "Sat Jul 23 13:26:30 CEST 2005"
 
 /* build host name */
 #define BUILD_HOST "fobos.generacio.com"
@@ -101,7 +101,7 @@
 #define HAVE__BOOL 1
 
 /* Assertions are disabled when this is defined. */
-#define NDEBUG 1
+/* #undef NDEBUG */
 
 /* Name of package */
 #define PACKAGE "ccruncher"
