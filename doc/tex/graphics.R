ad5ab4b4c9db630b10b74f3e9ee04e8903d9cbc4
Gerard Torrent <gerard@generacio.com>
2013-04-27 17:00:11 +0000

    improved bond lgd graphic

--- doc/tex/graphics.R
+++ doc/tex/graphics.R
@@ -1,4 +1,7 @@
 
+#install.packages("rgl")
+library(rgl)
+
 setwd("./Pictures/")
 
 # ================================================
@@ -29,14 +32,12 @@ for(i in 2:n)
 }
 
 pdf(file="bond_pd1.pdf")
-plot(0:200, T[1:201,5], type='l', xlab="Time in years", 
-     ylab="PD", main="Probability of Default rating BB")
+plot(0:200, T[1:201,5], type='l', xlab="Time in years", ylab="PD")
 grid()
 dev.off();
 
 pdf(file="bond_pd2.pdf")
-plot(0:10, T[1:11,5], type='l', xlab="Time in years", 
-     ylab="PD", main="Probability of Default rating BB")
+plot(0:10, T[1:11,5], type='l', xlab="Time in years", ylab="PD")
 axis(1, at=0:10)
 grid()
 dev.off();
@@ -47,8 +48,7 @@ ead = rev(cumsum(rev(cash)))
 ead = c(ead,ead[10])
 
 pdf(file="bond_ead.pdf")
-plot(0:10, ead, type='s', xlab="Time in years", 
-     ylab="EAD", main="Exposure At Default")
+plot(0:10, ead, type='s', xlab="Time in years", ylab="EAD")
 axis(1, at=0:10)
 grid()
 dev.off();
@@ -56,23 +56,60 @@ dev.off();
 x = 0:100/100
 t = 0:10
 f <- function(x,t) {
-dbeta(x,5,2)
+  dbeta(x,5,2)
 }
 z<-outer(x,t,f)
-pdf(file="bond_lgd.pdf")
+
+#pdf(file="bond_lgd.pdf")
 # see http://gallery.r-enthusiasts.com/graph/Normal_density_surface_42
-persp(x, t, z, 
-  theta=30, phi=20,
-  r=50, d=0.1,
-  expand=0.5,
-  ltheta=90, lphi=180,
+#persp(x, t, z, 
+#  theta=30, phi=20,
+#  r=50, d=0.1,
+#  expand=0.5,
+#  ltheta=90, lphi=180,
   #shade=0.75,
-  ticktype="detailed",
-  nticks=5,
+#  ticktype="detailed",
+#  nticks=5,
+#  xlab="LGD",
+#  ylab="Time in years",
+#  zlab="Density",
+#  main="Loss Given Default")
+#dev.off();
+
+# 3d params obtained using par3d(no.readonly=TRUE)
+par3d(
+  FOV=30,
+  ignoreExtent=FALSE,
+  mouseMode=c("trackball", "zoom", "fov"),
+  skipRedraw=FALSE,
+  userMatrix = t(matrix(ncol=4,nrow=4,data=c(
+  0.7334787,  0.6786929, 0.03721328,    1,
+ -0.1770330,  0.1378910, 0.97449744,    1,
+  0.6562532, -0.7213611, 0.22129118,    0,
+  0.0000000,  0.0000000, 0.00000000,    1))),
+  scale=c(5.9732680, 0.5973268, 2.4305289),
+  zoom=1.157625,
+  windowRect=c(387, 55, 943, 567),
+  family="sans",
+  font=1,
+  cex=1,
+  useFreeType=TRUE
+)
+
+plot3d(x, t, z, type='n', axes=FALSE, xlab="", ylab="", zlab="")
+for(i in 1:10) {
+  lines3d(x, i, dbeta(x,5,2))
+}
+axis3d('x--', labels=seq(0,1,0.2))
+axis3d('y+-', labels=seq(0,10,1), nticks=11)
+mtext3d("Time in years", 'y+-', pos=c(1.2,5,-0.5))
+axis3d('z', labels=seq(0,2.5,0.5))
+grid3d(side=c('z'))
+title3d(
   xlab="LGD",
-  ylab="Time in years",
-  zlab="probability",
-  main="Loss Given Default")
-dev.off();
+  ylab="",
+  zlab="Density")
+
+rgl.postscript("bond_lgd.pdf", fmt="pdf", drawText=TRUE)
 
 
