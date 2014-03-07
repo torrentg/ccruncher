884f86ec7bd5d06e6e48c93d337f2e42185af845
Gerard Torrent <gerard@generacio.com>
2014-03-07 10:06:31 +0000

    updated tag ccruncher-2.4 to version 2.4.1

--- src/utils/config.h
+++ src/utils/config.h
@@ -2,7 +2,7 @@
 /* src/utils/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* build date */
-#define BUILD_DATE "2014-02-21 10:38:44+01:00"
+#define BUILD_DATE "2014-03-07 10:17:33+01:00"
 
 /* build host name */
 #define BUILD_HOST "fobos.generacio.com"
@@ -116,7 +116,7 @@
 #define PACKAGE_NAME "ccruncher"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ccruncher 2.4.0"
+#define PACKAGE_STRING "ccruncher 2.4.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "ccruncher"
@@ -125,7 +125,7 @@
 #define PACKAGE_URL "http://www.ccruncher.net"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.4.0"
+#define PACKAGE_VERSION "2.4.1"
 
 /* Profiler instructions are added when this is defined. */
 /* #undef PROFILER */
@@ -137,7 +137,7 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "2.4.0"
+#define VERSION "2.4.1"
 
 /* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
@@ -160,6 +160,6 @@
 
 
 /* subversion identifier */
-#define SVN_VERSION "R1206"
+#define SVN_VERSION "R1212"
 
 
