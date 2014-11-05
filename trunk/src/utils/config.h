09f7b67b578fc6eae29d56b6cb42bb7f17a056e9
gerard <gerard@5129c4fb-51ec-0310-b567-9ce87c476161>
2014-11-05 06:11:58 +0000

    saved current work

--- trunk/src/utils/config.h
+++ trunk/src/utils/config.h
@@ -2,7 +2,7 @@
 /* src/utils/config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* build date */
-#define BUILD_DATE "2014-10-26 06:20:18+01:00"
+#define BUILD_DATE "2014-11-04 06:14:47+01:00"
 
 /* build host name */
 #define BUILD_HOST "fobos.generacio.com"
